6cea16960a42a26c6b432d153f669f2a0362a6c0
Wallyson Breno <62758339+wallycoding@users.noreply.github.com>
2023-03-11 14:14:42 -0300

    delete use.js

--- use.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = __importDefault(require("./lib/main"));
-(0, main_1.default)();
